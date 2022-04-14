e2de0ef66fb01cde21b2494eff3fbaebc9831d2d
Ricardo Boss <contact@ricardoboss.de>
2022-04-14 20:49:08 +0200

    Handle relative paths given

--- src/Commands/PublishCommand.php
+++ src/Commands/PublishCommand.php
@@ -10,9 +10,11 @@ use Elephox\Console\Command\Contract\CommandHandler;
 use Elephox\Files\Directory;
 use Elephox\Files\File;
 use Elephox\Files\FileAlreadyExistsException;
+use Elephox\Files\Path;
 use Elephox\Logging\Contract\Logger;
 use InvalidArgumentException;
 use RuntimeException;
+use Throwable;
 
 class PublishCommand implements CommandHandler
 {
@@ -30,8 +32,8 @@ class PublishCommand implements CommandHandler
 		$builder
 			->name('plane:publish')
 			->optional('parts', implode(',', self::DEFAULT_PARTS), 'The parts to publish (' . implode(', ', self::AVAILABLE_PARTS) . ')')
-			->optional('dockerDest', $this->env->root->getDirectory('docker')->getPath(), 'The directory to publish the docker files to.')
-			->optional('binDest', $this->env->root->getDirectory('bin')->getPath(), 'The directory to publish the Plane binary to.')
+			->optional('dockerDest', 'docker', 'The directory to publish the docker files to.')
+			->optional('binDest', 'bin', 'The directory to publish the Plane binary to.')
 			->optional('overwrite', false, 'Overwrite existing files')
 		;
 	}
@@ -39,8 +41,18 @@ class PublishCommand implements CommandHandler
 	public function handle(CommandInvocation $command): int|null
 	{
 		$parts = explode(',', $command->getArgument('parts')->value);
-		$dockerDestination = new Directory($command->getArgument('dockerDest')->value);
-		$binDestination = new Directory($command->getArgument('binDest')->value);
+		if (Path::isRooted($command->getArgument('dockerDest')->value)) {
+			$dockerDestination = new Directory($command->getArgument('dockerDest')->value);
+		} else {
+			$dockerDestination = $this->env->root->getDirectory($command->getArgument('dockerDest')->value);
+		}
+
+		if (Path::isRooted($command->getArgument('binDest')->value)) {
+			$binDestination = new Directory($command->getArgument('binDest')->value);
+		} else {
+			$binDestination = $this->env->root->getDirectory($command->getArgument('binDest')->value);
+		}
+
 		$overwrite = filter_var($command->getArgument('overwrite'), FILTER_VALIDATE_BOOL);
 
 		foreach ($parts as $part) {
@@ -83,7 +95,18 @@ class PublishCommand implements CommandHandler
 			throw new RuntimeException('Destination files are already present. Use --overwrite to force publishing the docker files.', previous: $e);
 		}
 
-		$composeFile = $this->env->root->getFile('docker-compose.yml');
-		$composeFile->putContents(str_replace(['./vendor/elephox/plane/runtimes/8.1'], [$destination->getPath()], $composeFile->getContents()));
+		try {
+			$composeFile = $this->env->root->getFile('docker-compose.yml');
+			$composeFile->putContents(
+				preg_replace(
+					'/^\.\/vendor\/elephox\/plane\/runtimes\/.+$/',
+					$destination->getPathRelative($this->env->root),
+					$composeFile->getContents()
+				)
+			);
+		} catch (Throwable) {
+			$this->logger->error("Failed to update docker-compose.yml");
+			$this->logger->error("You will need to update the docker-compose.yml file manually to point to the new Plane docker files at $destination");
+		}
 	}
 }
